f030e4060005cb34724ccd1890a275d987db54e0
youssef shebl <63800183+youssefshibl@users.noreply.github.com>
2023-10-01 21:32:19 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App  shebl"/>
+  <HelloWorld msg="Welcome to Your Vue.js App  -> new commit"/>
 </template>
 
 <script>
